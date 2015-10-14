287747efdfb5c951ede293fbe3c1d374ef5cf433
Farid Rn <faridv@gmail.com>
2015-10-15 01:00:11 +0330

    Added routes

--- api/libs/routes.php
+++ api/libs/routes.php
@@ -1,8 +1,56 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// GET route
+$app->get('/', function () {});
+$app->post('/', function () {});
+$app->put('/', function () {});
+$app->patch('/', function () {});
+$app->delete('/', function () {});
 
+// Users
+$app->get('/users', function () {
+    echo Helper::callProcedure('getUsers');
+});
+$app->get('/users/:id', function ($id) {
+    echo Helper::callProcedure("getUsersById", $id, 'int');
+});
+$app->post('/users', function () {
+    // add new user
+});
+$app->put('/users/:id', function ($id) {
+    // update user with id = $id
+});
+$app->delete('/users/:id', function ($id) {
+    // delete user with id = $id
+});
+
+// Types
+$app->get('/types', function () {
+    // return all types
+});
+$app->get('/types/:name', function ($name) {
+    // return type with name = $name
+});
+$app->post('/types', function () {
+    // add new type
+});
+$app->put('/types/:id', function ($id) {
+    // update type with id = $id
+});
+$app->delete('/types/:id', function ($id) {
+    // delete type with id = $id
+});
+
+// Emails
+$app->get('/emails', function () {});
+$app->get('/emails/:id', function ($id) {});
+$app->post('/emails', function () {});
+$app->put('/emails/:id', function ($id) {});
+$app->delete('/emails/:id', function ($id) {});
+
+// Newsletters
+$app->get('/newsletters', function () {});
+$app->get('/newsletters/:id', function ($id) {});
+$app->post('/newsletters', function () {});
+$app->put('/newsletters/:id', function ($id) {});
+$app->delete('/newsletters/:id', function ($id) {});
