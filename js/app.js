ca522443a870c4523b81aed2cc5b3378b34ea425
Farid Rn <faridv@gmail.com>
2015-10-20 21:24:55 +0330

    Working on App

--- js/app.js
+++ js/app.js
@@ -5,17 +5,23 @@ app.constant('Services', (function () {
     return {
         users: api + 'users/:id'
         , newsletters: api + 'newsletters/:id'
+        , types: api + 'types/:name'
     };
 })());
 
-app.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/n', {
-            controller: 'mainController',
-            templateUrl: 'tmpl/newsletters.html'
-        }).when('/', {
-            controller: 'usersController',
-            templateUrl: 'tmpl/users.html'
-        }).otherwise({redirectTo: '/'});
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider
+                .when('/', {controller: 'mainController', templateUrl: 'tmpl/newsletters.html'})
+                .when('/users', {controller: 'usersController', templateUrl: 'tmpl/users.html'})
+                .when('/logs', {controller: 'logsController', templateUrl: 'tmpl/logs.html'})
+                .otherwise({redirectTo: '/'});
+        if (window.history && window.history.pushState) {
+            $locationProvider.html5Mode({
+                enabled: true
+                , requireBase: false
+                , rewriteLinks: false
+            });
+        }
     }
 ]);
 
@@ -29,9 +35,23 @@ app.factory('Factory', ['$resource', 'Services', function ($resource, Services) 
                 all: {method: 'GET', isArray: true}
                 , get: {method: 'GET', params: {id: 0}, isArray: true}
             })
+            , Types: $resource(Services.types, {}, {
+                all: {method: 'GET', isArray: true}
+                , get: {method: 'GET', params: {name: ''}, isArray: true}
+            })
         };
     }
 ]);
+app.controller('menuController', function ($scope, $location) {
+    $scope.items = [
+        {link: '#/', icon: 'email', text: 'Newsletters'}
+        , {link: '/users', icon: 'face', text: 'Users'}
+        , {link: '#/logs', icon: 'list', text: 'Logs'}
+    ];
+    $scope.go = function ($path) {
+        $location.path($path);
+    };
+});
 app.controller('mainController', function ($scope) {
     $scope.message = 'Hello!';
 });
@@ -39,12 +59,26 @@ app.controller('usersController', ['$scope', 'Factory', function ($scope, Factor
         $scope.users = Factory.Users.all();
         // Methods
         $scope.getUser = function (user_id) {
-            $scope.surrentUser = Factory.Users.get({id: user_id});
-            console.log($scope.surrentUser);
+            $scope.user = Factory.Users.get({id: user_id});
+            console.log($scope.user);
         };
     }
 ]);
 app.controller('newslettersController', ['$scope', 'Factory', function ($scope, Factory) {
-        $scope.nresletters = Factory.Newsletters.all();
+        $scope.newsletters = Factory.Newsletters.all();
+        // Methods
+        $scope.getNewsletter = function (newsletter_id) {
+            $scope.newsletter = Factory.Newsletters.get({id: newsletter_id});
+            console.log($scope.newsletter);
+        };
+    }
+]);
+app.controller('typesController', ['$scope', 'Factory', function ($scope, Factory) {
+        $scope.types = Factory.Types.all();
+        // Methods
+        $scope.getType = function (type_name) {
+            $scope.type = Factory.Types.get({name: type_name});
+            console.log($scope.type);
+        };
     }
 ]);
